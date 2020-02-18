b790838399002945075a8410f483360d097e92a8
Mariusz <mariusz.szczepanik.lublin@gmail.com>
2020-02-18 12:07:45 +0100

    minor changes

--- app/src/main/java/com/marteczek/todoappkotlin/activity/TodoListActivity.kt
+++ app/src/main/java/com/marteczek/todoappkotlin/activity/TodoListActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_todo_list.*
 
 class TodoListActivity : AppCompatActivity() {
 
+    // onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo_list)
